9de74397200d1c3c3a3beb706864eb2e5afb23b8
Washim Akram Khan <61159573+wak75@users.noreply.github.com>
2020-05-03 12:59:25 +0530

    javascript_basics_2

---
+++ javascript_basics_2/page1.js
@@ -0,0 +1,9 @@
+function baby(){
+    var photo = document.createElement('img') ;
+    var div = document.getElementById('generate_baby');
+
+    photo.src="Cute-asian-baby-Gif-_tu63.gif";
+
+
+    div.appendChild(photo);
+}
